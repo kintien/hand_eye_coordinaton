bed87bbce16b8ae012fd7a15e9362a736af130ad
kintien <kintien@gmail.com>
2017-07-26 09:45:26 -0700

    iteration 3
    set change of lights based on light in room

--- Hand-eye/Hand-eye.ino
+++ Hand-eye/Hand-eye.ino
@@ -1,6 +1,8 @@
 #include <Adafruit_CircuitPlayground.h>
 //make variable value
 int value;
+int lightRecieved;
+int turnpoint;
 
 void setup() {
  //initialize serial monitor 
@@ -19,6 +21,24 @@ void setup() {
   CircuitPlayground.setPixelColor(8, 255, 255 , 255);
   CircuitPlayground.setPixelColor(9, 255, 255, 255);
    //delay 1/10 of a sec
+   
+int lightRecieved = CircuitPlayground.lightSensor();
+if(lightRecieved > 900 and lightRecieved < 1000){ turnpoint = 600;}
+if(lightRecieved > 800 and lightRecieved < 899){ turnpoint = 500;}
+if(lightRecieved > 700 and lightRecieved < 799){ turnpoint = 450;}
+if(lightRecieved > 600 and lightRecieved < 699){ turnpoint = 400;}
+if(lightRecieved > 500 and lightRecieved < 599){ turnpoint = 300;}
+if(lightRecieved > 400 and lightRecieved < 499){ turnpoint = 250;}
+if(lightRecieved > 300 and lightRecieved < 399){ turnpoint = 200;}
+if(lightRecieved > 200 and lightRecieved < 299){ turnpoint = 100;}
+if(lightRecieved > 100 and lightRecieved < 199){ turnpoint = 70;} 
+if(lightRecieved > 50 and lightRecieved < 99){ turnpoint = 45;}
+if(lightRecieved > 30 and lightRecieved < 49){ turnpoint = 20;} 
+if(lightRecieved > 15 and lightRecieved < 29){ turnpoint = 10;}
+if(lightRecieved > 10 and lightRecieved < 14){ turnpoint = 5;}
+if(lightRecieved >= 0 and lightRecieved < 9){ turnpoint = 0;}
+
+
    delay(1000);
 
 }
@@ -33,9 +53,9 @@ void loop() {
 //starts the neopixels and clears all the colors  
   CircuitPlayground.clearPixels();
 //make the value the light sensor recieves assign it to variable value  
-  value = CircuitPlayground.lightSensor();
+ 
 //make a variable x assign a random value between 0-5000
-  int x =  random(0,5000);
+  int x =  random(1000,8000);
 //make x the delay so randomly between 0 & 5sec the following steps happen
  
   delay(x);
@@ -44,11 +64,11 @@ int var = 0;
  while(var < 1){ 
 
 
-
   value = CircuitPlayground.lightSensor();
   Serial.print("Light Sensor: ");
   Serial.println(value);
-  if(value < 180){
+  Serial.println(turnpoint);
+  if(value < turnpoint){
     //turn lights green
   CircuitPlayground.setPixelColor(0, 0,   255,   0);
   CircuitPlayground.setPixelColor(1, 0, 255,   0);
@@ -60,7 +80,7 @@ int var = 0;
   CircuitPlayground.setPixelColor(7,0, 255, 0);
   CircuitPlayground.setPixelColor(8, 0, 255 , 0);
   CircuitPlayground.setPixelColor(9, 0, 255, 0);
-  CircuitPlayground.playTone(500, 100);
+ // CircuitPlayground.playTone(500, 100);
   var = 1;
   }
   else{
